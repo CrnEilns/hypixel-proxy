9ccee8315d74b24a7695e47d40205f9de18add5a
CrnEilns <crneilns@outlook.com>
2023-03-17 23:33:31 +0800

    modified:   ZBProxy.service 	modified:   main.go 	modified:   version/version.go

--- main.go
+++ main.go
@@ -25,13 +25,8 @@ func main() {
 	log.SetOutput(color.Output)
 	rand.Seed(time.Now().UnixNano())
 	console.SetTitle(fmt.Sprintf("ZBProxy %v | Running...", version.Version))
-	console.Println(color.HiRedString(` ______  _____   _____   _____    _____  __    __ __    __
-|___  / |  _  \ |  _  \ |  _  \  /  _  \ \ \  / / \ \  / /
-   / /  | |_| | | |_| | | |_| |  | | | |  \ \/ /   \ \/ /`), color.HiWhiteString(`
-  / /   |  _  { |  ___/ |  _  /  | | | |   }  {     \  /
- / /__  | |_| | | |     | | \ \  | |_| |  / /\ \    / /
-/_____| |_____/ |_|     |_|  \_\ \_____/ /_/  \_\  /_/`))
-	color.HiGreen("Welcome to ZBProxy %s (%s)!\n", version.Version, version.CommitHash)
+	console.Println(color.HiRedString(`HELLO `), color.HiWhiteString(`- HYP PORXY`))
+	color.HiGreen("Welcome to Proxy %s (%s)!\n", version.Version, version.CommitHash)
 	color.HiBlack("Build Information: %s, %s/%s, CGO %s\n",
 		runtime.Version(), runtime.GOOS, runtime.GOARCH, common.CGOHint)
 	// go version.CheckUpdate()
